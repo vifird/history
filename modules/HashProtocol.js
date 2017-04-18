68b484bdce7f5ee249a92c8aa981d8dfa6fdf229
vifird <vifird@163.com>
2017-04-19 01:04:00 +0800

    使HashHistory支持无key和有key自动识别。

--- modules/HashProtocol.js
+++ modules/HashProtocol.js
@@ -20,15 +20,28 @@ const getHashPath = () => {
   return hashIndex === -1 ? '' : href.substring(hashIndex + 1)
 }
 
-const pushHashPath = (path) =>
-  window.location.hash = path
+const supportStateFunction = ()=> {
+  return typeof(window.history.pushState) === 'function' && typeof(window.history.replaceState) === 'function'
+}
 
-const replaceHashPath = (path) => {
-  const hashIndex = window.location.href.indexOf('#')
+const pushHashPath = (path, state) =>{
+  if(supportStateFunction()) {
+    window.history.pushState(state, undefined, '#' + path)
+  }else {
+    window.location.hash = path
+  }
+}
 
-  window.location.replace(
-    window.location.href.slice(0, hashIndex >= 0 ? hashIndex : 0) + '#' + path
-  )
+const replaceHashPath = (path, state) => {
+  if(supportStateFunction()) {
+    window.history.replaceState(state, undefined, '#' + path)
+  }else {
+    const hashIndex = window.location.href.indexOf('#')
+
+    window.location.replace(
+      window.location.href.slice(0, hashIndex >= 0 ? hashIndex : 0) + '#' + path
+    )
+  }
 }
 
 export { getUserConfirmation, go } from './BrowserProtocol'
@@ -36,11 +49,12 @@ export { getUserConfirmation, go } from './BrowserProtocol'
 export const getCurrentLocation = (pathCoder, queryKey) => {
   let path = pathCoder.decodePath(getHashPath())
   const key = getQueryStringValueFromPath(path, queryKey)
-
   let state
   if (key) {
     path = stripQueryStringValueFromPath(path, queryKey)
     state = readState(key)
+  } else {
+    state = window.history.state
   }
 
   const init = parsePath(path)
@@ -89,27 +103,27 @@ const updateLocation = (location, pathCoder, queryKey, updateHash) => {
 
   let path = pathCoder.encodePath(createPath(location))
 
-  if (state !== undefined) {
+  if (state !== undefined && typeof window.history.pushState !== 'function') {
     path = addQueryStringValueToPath(path, queryKey, key)
     saveState(key, state)
   }
 
   prevLocation = location
 
-  updateHash(path)
+  updateHash(path, state)
 }
 
 export const pushLocation = (location, pathCoder, queryKey) =>
-  updateLocation(location, pathCoder, queryKey, (path) => {
+  updateLocation(location, pathCoder, queryKey, (path, state) => {
     if (getHashPath() !== path) {
-      pushHashPath(path)
+      pushHashPath(path, state)
     } else {
       warning(false, 'You cannot PUSH the same path using hash history')
     }
   })
 
 export const replaceLocation = (location, pathCoder, queryKey) =>
-  updateLocation(location, pathCoder, queryKey, (path) => {
+  updateLocation(location, pathCoder, queryKey, (path, state) => {
     if (getHashPath() !== path)
-      replaceHashPath(path)
+      replaceHashPath(path, state)
   })
